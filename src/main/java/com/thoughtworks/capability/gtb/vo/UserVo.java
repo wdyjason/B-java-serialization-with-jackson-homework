ec540b9b62ae86eab991c4b33443a48bd4aa4da3
wdyjason <ddd123321456555@qq.com>
2020-09-11 18:51:53 +0800

    finish get right value at EventController

--- src/main/java/com/thoughtworks/capability/gtb/vo/UserVo.java
+++ src/main/java/com/thoughtworks/capability/gtb/vo/UserVo.java
@@ -1,5 +1,7 @@
 package com.thoughtworks.capability.gtb.vo;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,8 +9,10 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonPropertyOrder({"userId", "name"})
 public class UserVo {
 
+  @JsonProperty("userId")
   private String id;
   private String name;
 }
